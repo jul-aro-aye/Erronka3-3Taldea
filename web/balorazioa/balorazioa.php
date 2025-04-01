0f8dcb1c05f7ba7b5cf173d0bf9595f05f95597e
Alexlizmug <ale_liz_mug@goierrieskola.org>
2025-04-01 10:05:11 +0200

    Balorazio orria-ren itxuraren bigarren zatia eginda

--- web/balorazioa/balorazioa.php
+++ web/balorazioa/balorazioa.php
@@ -21,7 +21,7 @@ $result = $conn->query($sql);
     <div class="content-osoa">
         <h1 id="enpresaIzena">AeroPark</h1>
         <form action="" method="POST" id="balorazioaForm">
-            <label for="barrakaIzena" class="barrakaIzena">Aukeratu barraka bat:</label>
+            <label for="barrakaIzena" id="barrakaIzena">Aukeratu barraka bat:<span class="asterisco">*</span></label>
             <select name="barrakaIzena" class="barrakaIzena">
                 <option value="">-- Aukeratu --</option>
                 <?php
@@ -37,7 +37,7 @@ $result = $conn->query($sql);
  
             </select>
             <br>
-            <label for="balorazioa" id="balorazioa">Balorazioa<span class="asterisco">*</span>:
+            <label for="balorazioa" id="balorazioa">Balorazioa<span class="asterisco">*</span>
                 <div class="rating">
                     <span class="star" data-value="1">&#9733;</span>
                     <span class="star" data-value="2">&#9733;</span>
@@ -48,9 +48,40 @@ $result = $conn->query($sql);
                 <div id="ratingResult"></div>
                 <div class="hidden" id="ratingValue"></div>
             </label>
+            <br>
+            <label for="iruzkina" id="iruzkina">Iruzkinak:<span class="asterisco">*</span></label>
+            <br>
+            <textarea name="iruzkina" id="iruzkina" rows="4" cols="20" placeholder="Idatzi iruzkina"></textarea>
+            <br><br>
+            <button class="baloratuBotoia">Bidali</button>
         </form>
     </div>
     <?php require_once "../footer.php"; ?>
+ 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+ 
+    <script>
+        $(document).ready(function () {
+            $(".star").click(function () {
+                let rating = $(this).data("value"); // Obtiene el valor de la estrella clickeada
+                $("#ratingValue").text(rating); // Muestra el valor seleccionado (puedes enviarlo en un formulario)
+ 
+                // Ilumina todas las estrellas hasta la seleccionada
+                $(".star").each(function () {
+                    let starValue = $(this).data("value");
+                    if (starValue <= rating) {
+                        $(this).css("color", "gold");
+                    } else {
+                        $(this).css("color", "black");
+                    }
+                });
+            });
+        });
+ 
+    </script>
+ 
 </body>
+ 
+ 
  
 </html>
