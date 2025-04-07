ec1816f8b06dcd29f0de3a9ba3615c722ea19ee4
Alain-Afonso-Redondo <alainafonsoredondo@gmail.com>
2025-04-07 09:58:17 +0200

    Erregistro orrian telefonoa sartzeko funtzioa zuzenduta

--- web/erregistroOrria/erregistroOrria.php
+++ web/erregistroOrria/erregistroOrria.php
@@ -78,7 +78,7 @@ require_once("../konfigurazioa/layoutTop.php");
                         "abizenak": abizenval,
                         "erabiltzailea": erabiltzaileval,
                         "pasahitza": pasahitzval,
-                        "telefono": telefonoval,
+                        "telefonoa": telefonoval,
                         "emaila": emailval,
                     }
                 })
